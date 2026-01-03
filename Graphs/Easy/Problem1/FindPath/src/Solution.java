f194713c7774ed693fac52f93c371ce7615b0c3a
NaiveDeveloper01 <156093463+NaiveDeveloper01@users.noreply.github.com>
2026-01-03 10:41:48 +0530

    Shubham|Graphs|Approach 2 using Disjoint union (DSU).

--- Graphs/Easy/Problem1/FindPath/src/Solution.java
+++ Graphs/Easy/Problem1/FindPath/src/Solution.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
+    //------------------------- DFS approach ------------------------------------
+    public boolean validPath1(int n, int[][] edges, int source, int destination) {
         Map<Integer, List<Integer>> adjacencyList = new HashMap<>();
         for(int [] edge : edges){
             List<Integer> list1 = adjacencyList.getOrDefault(edge[0], new ArrayList<>());
@@ -31,5 +32,34 @@ class Solution {
             }
         }
         return false;
+    }
+//-------------------------------------- Disjoint union approach ---------------------------------
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Map<Integer,Integer> parent = new HashMap<>();
+        for(int [] edge : edges){
+            union(edge[0],edge[1],parent);
+        }
+
+        return find(source,parent) == find(destination,parent);
+    }
+    private void union(int start, int end, Map<Integer,Integer> parent){
+        int startRoot = find(start,parent);
+        int endRoot = find(end,parent);
+
+        if(startRoot != endRoot){
+            parent.put(startRoot,endRoot);
+        }
+    }
+    private int find(int node, Map<Integer,Integer> parent){
+        makeSet(node,parent);
+        if(!parent.get(node).equals(node)){
+            parent.put(node,find(parent.get(node),parent));
+        }
+        return parent.get(node);
+    }
+    private void makeSet(int node,Map<Integer,Integer> parent){
+        if(!parent.containsKey(node)){
+            parent.put(node,node);
+        }
     }
 }
